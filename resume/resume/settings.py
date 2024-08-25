2f7823e8aedfabb96d5ba126c4c0b9792cbbf162
Ryan Smith <smith.ryanr@gmail.com>
2024-08-24 22:15:47 -0500

    Fix attempt 1

--- resume/resume/settings.py
+++ resume/resume/settings.py
@@ -111,7 +111,7 @@ STATICFILES_DIRS = ['static/']
 
 # Push site online
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATID_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
